465bbbe799f671d4419474eafb60333fb90a16d7
Thiago <thiagorocha2789@gmail.com>
2025-04-04 19:55:00 -0400

    Primeiro Comit

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+export default function Index () {
+    return  (
+     <View>
+        <Text>
+        Eu sou o Milior!!
+        </Text>
+     </View>
+    );
+}
