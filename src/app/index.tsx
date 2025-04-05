1bdb4beaa50801af4b1a1a0ee79577ff3f614628
Thiago <thiagorocha2789@gmail.com>
2025-04-05 17:16:53 -0300

    Adicionanando Style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { blue, red } from "react-native-reanimated/lib/typescript/Colors";
 
 export default function index () {
 return (
 <View>
-    <Text>eu sou o pior!!</Text>
-    <Text>Pobre so atrai problemas</Text>
-    <Text>Você é Burro</Text>
+    <Text style= {styles.title} >eu sou o pior</Text>
+
+    <Text style= {styles.title2}>Pobre so atrai problemas</Text>
+    <Text style= {styles.title3}>Você é Burro</Text>
 </View>
 );
 
 
 }
+
+const styles = StyleSheet.create({
+    title: {
+        color: "red",
+        fontSize: 22,
+    },
+
+    title2: {
+        color: "red",
+        fontSize: 26,
+    },
+
+    title3: {
+        color: "red",
+        fontSize:30
+    }
+
+})
