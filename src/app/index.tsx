f64806fa72627f7b3be5c44ffa0c0f4765723a31
Thiago <thiagorocha2789@gmail.com>
2025-04-05 15:41:28 -0300

    Inserindo Elementos

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+export default function index () {
+return (
+<View>
+    <Text>eu sou o pior!!</Text>
+    <Text>Pobre so atrai problemas</Text>
+    <Text>Você é Burro</Text>
+</View>
+);
+
+
+}
